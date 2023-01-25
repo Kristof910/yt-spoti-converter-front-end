ca186a44b3306f0b75c697dddbfd0f5dc0c12ebc
Bába Kristóf <kristof@Babas-MacBook-Pro.local>
2023-01-25 15:45:02 +0200

    UI updated

--- main.py
+++ main.py
@@ -9,68 +9,84 @@ app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def main():
-    if request.method == 'POST':
-        print("SUCCESFULL")
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return render_template('error.html')    
 
 @app.route("/connect")
 def connect():
-    auth_url = f'https://accounts.spotify.com/authorize?client_id={setup.spoti_client_id}&response_type=code&redirect_uri=http://localhost:9874/callback&scope=playlist-modify-public user-read-private user-library-read'
-    return redirect(auth_url)
+    try:
+        auth_url = f'https://accounts.spotify.com/authorize?client_id={setup.spoti_client_id}&response_type=code&redirect_uri=http://localhost:9874/callback&scope=playlist-modify-public user-read-private user-library-read'
+        return redirect(auth_url)
+    except Exception as e:
+        return render_template('error.html')      
 
 @app.route("/callback")
 def callback():
-    code = request.args.get('code')
+    try:
+        code = request.args.get('code')
 
-    token_url = 'https://accounts.spotify.com/api/token'
-    data = {'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': 'http://localhost:9874/callback'}
-    auth = (setup.spoti_client_id, setup.spoti_client_secret)
-    response = requests.post(token_url, data=data, auth=auth)
-    access_token = response.json()['access_token']
+        token_url = 'https://accounts.spotify.com/api/token'
+        data = {'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': 'http://localhost:9874/callback'}
+        auth = (setup.spoti_client_id, setup.spoti_client_secret)
+        response = requests.post(token_url, data=data, auth=auth)
+        access_token = response.json()['access_token']
 
-    session['access_token'] = access_token
-    return redirect('/nextstep')
+        session['access_token'] = access_token
+        return redirect('/nextstep')
+    except Exception as e:
+        return render_template('error.html')      
 
 @app.route("/nextstep")
 def nextstep():
-    return render_template("nextstep.html")
+    try:
+        return render_template("nextstep.html")
+    except Exception as e:
+        return render_template('error.html')      
 
 @app.route("/nextstep", methods=["POST"])
 def yt_link():
-    text = request.form['yt_link']
-    
-    processed_link = ""
-    index = 0
-    if text[0] == ("h" or "H"):
-        index = 38
-    elif text[0] == ("w" or "W"):
-        index = 30
-    elif text[0] == ("y" or "Y"):
-        index = 26        
-    while index < len(text):
-        processed_link += text[index]
-        index += 1
+    try:
+        text = request.form['yt_link']
+        
+        processed_link = ""
+        index = 0
+        if text[0] == ("h" or "H"):
+            index = 38
+        elif text[0] == ("w" or "W"):
+            index = 30
+        elif text[0] == ("y" or "Y"):
+            index = 26        
+        while index < len(text):
+            processed_link += text[index]
+            index += 1
 
-    session["yt_link"] = processed_link
-    return redirect("/finalstep")
+        session["yt_link"] = processed_link
+        return redirect("/finalstep")
+    except Exception as e:
+        return render_template('error.html')      
 
 @app.route("/finalstep")
 def finalstep():
-    headers = {
-        "Authorization": "Bearer " + session["access_token"]
-    }
-    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
-    user_id = json.loads(response.text)["id"]
+    try:
+        headers = {
+            "Authorization": "Bearer " + session["access_token"]
+        }
+        response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+        user_id = json.loads(response.text)["id"]
 
-    info = {
-        "yt_playlist" : session["yt_link"], 
-        "spoti_token" : session["access_token"], 
-        "spoti_user_id" : user_id
-    }
-    requests.post("http://localhost:9873/endpoint", json=info)
-    return render_template("finalstep.html")
+        info = {
+            "yt_playlist" : session["yt_link"], 
+            "spoti_token" : session["access_token"], 
+            "spoti_user_id" : user_id
+        }
+        requests.post("http://localhost:9873/endpoint", json=info)
+        return render_template("finalstep.html")
+    except Exception as e:
+        return render_template('error.html')      
 
 if __name__ == '__main__':
     app.run(host='localhost', port=9874)
