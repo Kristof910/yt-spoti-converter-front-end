7d1b7d2d29a2d553cae216b10c43f119876ab67d
Bába Kristóf <kristof@Babas-MacBook-Pro.local>
2023-01-26 15:20:50 +0200

    small fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session
 import requests
 import json
+import threading
 
 import setup
 
@@ -10,8 +11,11 @@ app.secret_key = 'your_secret_key'
 @app.route('/')
 def main():
     try:
+        if(setup.spoti_client_id == "" or setup.spoti_client_secret == ""):
+            raise Exception("setup.py is not filled")
         return render_template('index.html')
     except Exception as e:
+        print(f"Ops, something gone wrong in main: {e}")
         return render_template('error.html')    
 
 @app.route("/connect")
@@ -20,6 +24,7 @@ def connect():
         auth_url = f'https://accounts.spotify.com/authorize?client_id={setup.spoti_client_id}&response_type=code&redirect_uri=http://localhost:9874/callback&scope=playlist-modify-public user-read-private user-library-read'
         return redirect(auth_url)
     except Exception as e:
+        print(f"Ops, something gone wrong in connect: {e}")
         return render_template('error.html')      
 
 @app.route("/callback")
@@ -38,6 +43,7 @@ def callback():
         session['access_token'] = access_token
         return redirect('/nextstep')
     except Exception as e:
+        print(f"Ops, something gone wrong in callback: {e}")
         return render_template('error.html')      
 
 @app.route("/nextstep")
@@ -45,6 +51,7 @@ def nextstep():
     try:
         return render_template("nextstep.html")
     except Exception as e:
+        print(f"Ops, something gone wrong in nextstep: {e}")
         return render_template('error.html')      
 
 @app.route("/nextstep", methods=["POST"])
@@ -54,7 +61,9 @@ def yt_link():
         
         processed_link = ""
         index = 0
-        if text[0] == ("h" or "H"):
+        if (text[0] == ("h" or "H")) and (text[8] == ("y" or "Y")):
+            index = 34
+        elif text[0] == ("h" or "H"):
             index = 38
         elif text[0] == ("w" or "W"):
             index = 30
@@ -67,6 +76,7 @@ def yt_link():
         session["yt_link"] = processed_link
         return redirect("/finalstep")
     except Exception as e:
+        print(f"Ops, something gone wrong in yt_link: {e}")
         return render_template('error.html')      
 
 @app.route("/finalstep")
@@ -83,10 +93,14 @@ def finalstep():
             "spoti_token" : session["access_token"], 
             "spoti_user_id" : user_id
         }
-        requests.post("http://localhost:9873/endpoint", json=info)
+        
+        thread = threading.Thread(target=requests.post, args=("http://localhost:9873/endpoint",), kwargs={"json":info})
+        thread.start()
         return render_template("finalstep.html")
+        
     except Exception as e:
-        return render_template('error.html')      
+        print(f"Ops, something gone wrong in finalstep: {e}")
+        return render_template('error.html')   
 
 if __name__ == '__main__':
     app.run(host='localhost', port=9874)
